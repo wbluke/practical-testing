f10fc6307a2ac7d981361d8df44007fcf4831f6c
wbluke <pwb0127@gmail.com>
2023-01-14 13:46:30 +0900

    project init

---
+++ src/main/java/sample/cafekiosk/CafekioskApplication.java
@@ -0,0 +1,13 @@
+package sample.cafekiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafekioskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CafekioskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sample/cafekiosk/CafekioskApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.cafekiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafekioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
